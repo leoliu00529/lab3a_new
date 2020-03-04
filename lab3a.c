cee304b1cc2468e29798ef9f02d15dad46766975
Zihan Liu <45219877+leoliu00529@users.noreply.github.com>
2020-03-04 13:53:25 -0800

    Add files via upload

---
+++ lab3a.c
@@ -0,0 +1,294 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <string.h>
+#include "ext2.h"
+
+FILE * imfd;
+FILE * outfd;
+
+int total_block;
+int total_inode;
+int block_size;
+int inode_size;
+int block_per_group;
+int inode_per_group;
+
+int group_number = 0;
+int num_of_free_block;
+int num_of_free_inode;
+int block_bitmap;
+int inode_bitmap;
+int first_nonres_ino;
+
+int inode_table;
+
+void sysError(char * message) {
+    fprintf(stderr, "%s\n", message);
+    exit(2);
+}
+
+void superBlock() {
+    struct ext2_super_block ext2superblock;
+    
+    // move current position to 1024
+    fseek(imfd, 1024, SEEK_SET);
+
+    // read 1024 bytes
+    fread(&ext2superblock, 1024, 1, imfd);
+    if(ferror(imfd)){
+        sysError("Failed to read superblock.");
+    }
+
+    total_block = ext2superblock.s_blocks_count;
+    total_inode = ext2superblock.s_inodes_count;
+    block_size = 1024 << ext2superblock.s_log_block_size;
+    inode_size = ext2superblock.s_inode_size;
+    block_per_group = ext2superblock.s_blocks_per_group;
+    inode_per_group = ext2superblock.s_inodes_per_group;
+    first_nonres_ino = ext2superblock.s_first_ino;
+
+    fprintf(outfd, "SUPERBLOCK,%d,%d,%d,%d,%d,%d,%d\n", total_block, total_inode, block_size, inode_size, block_per_group, inode_per_group, first_nonres_ino);
+
+    return;
+}
+
+void group_desc()
+{
+  struct ext2_group_desc group;
+  fread(&group, 32, 1, imfd);
+  if(ferror(imfd)){
+    sysError("Failed to read group descriptor.");
+  }
+
+  num_of_free_block = group.bg_free_blocks_count;
+  num_of_free_inode = group.bg_free_inodes_count;
+  block_bitmap = group.bg_block_bitmap;
+  inode_bitmap = group.bg_inode_bitmap;
+  inode_table = group.bg_inode_table;
+
+  fprintf(outfd, "GROUP,%d,%d,%d,%d,%d,%d,%d,%d\n", group_number, total_block, total_inode, num_of_free_block, num_of_free_inode, block_bitmap, inode_bitmap, inode_table);
+
+  return;
+}
+
+void bFree()
+{
+  fseek(imfd, block_bitmap * block_size, SEEK_SET);
+  char* bitmap = (char*)malloc(sizeof(char) * block_size);
+  if(bitmap == NULL)
+    {
+      sysError("Failed to allocate memory.");
+    }
+  fread(bitmap, 1, block_size, imfd);
+  if(ferror(imfd)){
+    sysError("Failed to read block bitmap.");
+  }
+  int i, j;
+  char temp;
+  int count = 1;
+  for(i = 0; i < block_size; i++)
+    {
+      temp = bitmap[i] & 0xFF;
+      for(j = 0; j < 8; j++)
+    {
+      if(!((temp >> j) & 0x1))
+        {
+          fprintf(outfd, "BFREE, %d\n", count);
+        }
+      count++;
+    }
+    }
+  return;
+}
+
+void iFree()
+{
+  fseek(imfd, inode_bitmap * block_size, SEEK_SET);
+  char* bitmap = (char*)malloc(sizeof(char) * block_size);
+  if(bitmap == NULL)
+    {
+      sysError("Failed to allocate memory.");
+    }
+  fread(bitmap, 1, block_size, imfd);
+  if(ferror(imfd)){
+    sysError("Failed to read inode bitmap.");
+  }
+  int i, j;
+  char temp;
+  int count = 1;
+  for(i = 0; i < block_size; i++)
+    {
+      temp = bitmap[i] & 0xFF;
+      for(j = 0; j < 8; j++)
+        {
+          if(!((temp >> j) & 0x1))
+            {
+              fprintf(outfd, "IFREE, %d\n", count);
+            }
+          count++;
+        }
+    }
+  return;
+}
+
+void writetime(time_t t) {
+    struct tm *info;
+    info = localtime(&t);
+    
+    int year = 1900 + info->tm_year;
+    
+    fprintf(outfd, "%d/%d/%d %d/%d/%d,", info->tm_mon, info->tm_mday, year, info->tm_hour, info->tm_min, info->tm_sec);
+    
+    return;
+}
+
+void i_summary() {
+    struct ext2_inode ext2inode;
+    fseek(imfd, (inode_bitmap + 1) * block_size, SEEK_SET);
+    int i = 0;
+    for (; i < inode_per_group; i ++) {
+        fread(&ext2inode, 128, 1, imfd);
+        char inode_type;
+        if (ext2inode.i_mode != 0  && ext2inode.i_links_count != 0) {
+            fprintf(imfd, "%d"  , ext2inode.i_mode);
+            if ((ext2inode.i_mode & 0xA000)== 0xA000) {
+                inode_type = 's';
+            }
+            else if ((ext2inode.i_mode & 0x8000) == 0x8000) {
+                inode_type = 'f';
+            }
+            else if ((ext2inode.i_mode & 0x4000) == 0x4000) {
+                inode_type = 'd';
+            }
+            else {
+                inode_type = '?';
+            }
+            time_t atime = ext2inode.i_atime;
+            time_t mtime = ext2inode.i_mtime;
+            time_t ctime = ext2inode.i_ctime;
+            fprintf(outfd, "INODE,%d,%c,%o,%d,%d,%d,", i + 1, inode_type, ext2inode.i_mode & 0XFFF, ext2inode.i_uid, ext2inode.i_gid, ext2inode.i_links_count);
+            
+            writetime(ctime);
+            writetime(mtime);
+            writetime(atime);
+            
+            fprintf(outfd, "%d,%d", ext2inode.i_size, ext2inode.i_blocks);
+            
+            if (inode_type == 's' && ext2inode.i_blocks == 0) {
+                fprintf(outfd, "\n");
+            }
+            else if (inode_type == 'f' || inode_type == 'd' || inode_type == 's') {
+                fprintf(outfd, ",%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n", ext2inode.i_block[0], ext2inode.i_block[1], ext2inode.i_block[2], ext2inode.i_block[3], ext2inode.i_block[4], ext2inode.i_block[5], ext2inode.i_block[6], ext2inode.i_block[7], ext2inode.i_block[8], ext2inode.i_block[9], ext2inode.i_block[10], ext2inode.i_block[11], ext2inode.i_block[12], ext2inode.i_block[13], ext2inode.i_block[14]);
+            }
+            else {
+                fprintf(outfd, "\n");
+            }
+        }
+    }
+
+    return;
+}
+
+void directory_entries() {
+    struct ext2_inode ext2inode;
+    struct ext2_dir_entry ext2dirent;
+    
+    int i = 0;
+    for (; i < inode_per_group; i ++) {
+        fseek(imfd, (inode_bitmap + 1) * block_size + i * 128, SEEK_SET);
+        fread(&ext2inode, 128, 1, imfd);
+        if (ext2inode.i_mode != 0  && ext2inode.i_links_count != 0 && (ext2inode.i_mode & 0x4000) == 0x4000) {
+            unsigned int j = 0;
+            for (; j < ext2inode.i_blocks; j ++) {
+                int k = 0;
+                for(; k < 1024; k += ext2dirent.rec_len) {
+                    fseek(imfd, (ext2inode.i_block[j]) * 1024 + k, SEEK_SET);
+                    fread(&ext2dirent, sizeof(ext2dirent), 1, imfd);
+                    if (!ext2dirent.file_type || !ext2dirent.inode) {
+                        break;
+                    }
+                        
+                    char outputstr[ext2dirent.rec_len - 5];
+                    snprintf(outputstr, ext2dirent.rec_len - 5, "\'%s\'", ext2dirent.name);
+                    fprintf(outfd, "DIRENT,%d,%d,%d,%d,%d,%s\n", i + 1, k, ext2dirent.inode, ext2dirent.rec_len, ext2dirent.name_len, outputstr);
+                }
+            }
+        }
+    }
+    return;
+}
+
+void rec_indi(int block, int level, int parent_inode, int offset) {
+    fseek(imfd, block * 1024, SEEK_SET);
+    int address[256];
+    int i;
+    for (i = 0; i < 256; i ++) {
+        fread(&address[i], 8, 1, imfd);
+    }
+    for (i = 0; i < 256; i ++) {
+        if (address[i] != 0) {
+            fprintf(outfd, "INDIRECT,%d,%d,%d,%d,%d\n", parent_inode, level, offset, block, address[i]);
+            if (level != 1) {
+                rec_indi(address[i], level - 1, parent_inode, offset);
+            }
+        }
+        
+        offset += 256 ^ (level - 1);
+    }
+}
+
+void indirect() {
+    struct ext2_inode ext2inode;
+    fseek(imfd, (inode_bitmap + 1) * block_size, SEEK_SET);
+    int i = 0;
+    for (; i < inode_per_group; i ++) {
+        fread(&ext2inode, 128, 1, imfd);
+        if ((ext2inode.i_mode & 0x8000) == 0x8000 || (ext2inode.i_mode & 0x4000) == 0x4000){
+            if (ext2inode.i_block[12] != 0) {
+                rec_indi(ext2inode.i_block[12], 1, i + 1, 12);
+            }
+//            if (ext2inode.i_block[13] != 0) {
+//                rec_indi(ext2inode.i_block[13], 2, i + 1, 12);
+//            }
+//            if (ext2inode.i_block[14] != 0) {
+//                rec_indi(ext2inode.i_block[14], 3, i + 1, 12);
+//            }
+        }
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    if (argc != 2) {
+      fprintf(stderr,"Invalid argument(s).");
+      exit(1);
+    }
+    
+    imfd = fopen(argv[1], "r");
+    if (imfd == NULL) {
+        sysError("Failed to open the image file.");
+    }
+    
+    outfd = fopen("output.csv", "w+");
+    if (outfd == NULL) {
+        sysError("Failed to create output csv file.");
+    }
+    
+    superBlock();
+    group_desc();
+    bFree();
+    iFree();
+    i_summary();
+    directory_entries();
+    indirect();
+    
+    fclose(imfd);
+    fclose(outfd);
+   
+    exit(0);
+}
